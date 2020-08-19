8628218e13cd958a929b367635e6fc78bc352d3f
LOKENDRA SINGH HADA <lokendra.webvillee@gmail.com>
2020-08-19 09:25:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const mongoose = require('mongoose');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+mongoose.connect('mongodb://POS:12345@ds127443.mlab.com:27443/pos')
+  .then(()=>{
+    app.listen(8000);
+  })
+  .catch(err=>{
+    console.log(err)
+  })
